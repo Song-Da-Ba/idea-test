292f20e5746cd15b21f005cc10e85de31428df44
Song-Da-Ba <101404378+Song-Da-Ba@users.noreply.github.com>
2022-09-09 10:58:17 +0800

    pull commit

--- src/homework/GitTest.java
+++ src/homework/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello git2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
